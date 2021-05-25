844b19d045decfadb3f01d41e3b0f47576d8f910
Avirup Saha <sahaavirup1998@gmail.com>
2021-05-25 22:57:10 +0530

    countdown-timer

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component, useState, useEffect } from "react";
+//import "../styles/App.css";
+
+const App = () => {
+  // write your code here
+  const [time, setTime] = useState(0);
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    !valid &&
+      setTimeout(() => {
+        time > 0 && setTime(time - 1);
+      }, 1000);
+    valid && setTime(0);
+  }, [time]);
+
+  function decreamentCount(e) {
+    if (e.key === "Enter") {
+      // console.log(isNaN(e.target.value));
+      if (isNaN(e.target.value) || e.target.value == 0) {
+        setValid(true);
+      } else {
+        setValid(false);
+        setTime(Math.floor(e.target.value));
+      }
+    }
+  }
+
+  return (
+    <div className="wrapper">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyDown={decreamentCount /* callback function */}
+          />{" "}
+          sec.
+        </h1>
+      </div>
+      <div id="current-time">{valid ? 0 : time /* remaining time */}</div>
+    </div>
+  );
+};
+
+export default App;
